6fee075d8b9532d67917e6bb774179aee0f012c9
tokufxug <tokufxug@gmail.com>
2024-03-01 19:16:06 +0900

    Initial Commit

---
+++ VisionOSImageTrackingSample/VisionOSImageTrackingSampleApp.swift
@@ -0,0 +1,21 @@
+//
+//  VisionOSImageTrackingSampleApp.swift
+//  VisionOSImageTrackingSample
+//
+//  Created by Sadao Tokuyama on 3/1/24.
+//
+
+import SwiftUI
+
+@main
+struct VisionOSImageTrackingSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+
+        ImmersiveSpace(id: "ImmersiveSpace") {
+            ImmersiveView()
+        }
+    }
+}
